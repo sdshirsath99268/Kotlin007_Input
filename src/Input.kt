fbf80e477e0cf6679737f3cfd6d559a4353d0d38
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 21:07:21 +0530

    Initial commit

---
+++ src/Input.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+
+    println("Enter your Name :")
+    val name = readLine()
+
+    println("Enter your age")
+    val age = readLine()
+
+    println(name + " " + age)
+
+    println("Seventh Kotline Prog , By Sanjay D Shirsath")
+}
